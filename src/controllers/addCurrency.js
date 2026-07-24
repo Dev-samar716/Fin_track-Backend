45fdb646b4b7ef837231850fa653f23757af75a9
Samar Kasaudhan <samardevworks@gmail.com>
2026-07-24 16:00:29 +0545

    Added code to update existing currency preference if it exists, instead of creating a new one

--- src/controllers/addCurrency.js
+++ src/controllers/addCurrency.js
@@ -16,7 +16,7 @@ const addCurrency = async(req, res) => {
     try {
      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
      
-     if(!docoded) {
+     if(!decoded) {
         return res.status(401).json({
             success: false,
             message: "Unauthorized access: Invalid token!"
@@ -31,6 +31,29 @@ const addCurrency = async(req, res) => {
          message: "Server faced internal errors while trying to verfy token!"
         }) 
    }
+
+   // Check if a currency preference is already set
+
+   try {
+     const query = `SELECT * FROM currencies WHERE user_id=$1`
+    const user = await pool.query(query, [user_id]);
+
+     if(user.rows.length > 0) {
+        const updatePreferenceQuery = `UPDATE currencies SET currency_code=$1, currency_symbol=$2
+        WHERE user_id=$3 RETURNING *`
+
+        await pool.query(updatePreferenceQuery, [currency_code, currency_symbol, user_id]);
+         return res.status(201).json({
+                success: true,
+            })
+          } 
+     }  catch(error) {
+            console.log(error);
+           return res.status(500).json({
+                status: false,
+                message: "Failed to check if currency preference already exists!"
+            })
+          }
 
    // Add the currency preference in database with correct user identity
       
